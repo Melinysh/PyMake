9098f6ba8ca7c3c8a35800a84ba3b506a0e4399f
Stevo11 <smelinysh@icloud.com>
2015-05-06 18:41:55 -0400

    v0.1.3 Support for setting compiler explicitly.

--- README.md
+++ README.md
@@ -1,20 +1,31 @@
-##### PyMake
-#### A basic makefile generator
+# PyMake
+## A basic makefile generator
 
-Pymake generates a Makefile for the files of the current working directory. Currently works with C and C++ files and can use gcc or g++ for the compiler. 
+Pymake generates a Makefile for the files of the current working directory. Currently works with C/C++ files. You can declare your compiler or pymake will try to detect gcc or g++ for the compiler. 
 
-## Current version: 0.1.2 
+#### Current version: 0.1.3
 
-## Usage: pymake.py [-f "Flags" -o Output File -d Directory]  
 
-## Options:  
-  --version             show program's version number and exit  
-  -h, --help            show this help message and exit  
+####Usage: pymake.py [-f "Flags" -o Output File -d Directory -c Compiler]   
+
+Options:  
+
+  --version  
+show program's version number and exit  
+
+  -h, --help  
+show this help message and exit   
+ 
   -f Flags, --flags=Flags  
-                        Extra flags for the compiler. Typed within quotes. For
-                        example, "-Wall". Default is "-o [fileName]"  
+Extra flags for the compiler. Typed within quotes. For example, "-Wall". Default is "-o [fileName]"  
+
   -o outputFile, --output=outputFile  
-                        Output file name from compiler. Default is a.out 
-  -d directory, --directory=directory
-                          Directory for pymake to create Makefile for. Default
-						  is ./
+Output file name from compiler. Default is a.out    
+
+  -d directory, --directory=directory  
+Directory for pymake to create Makefile in. Default is ./  
+  
+  -c compiler, --compiler=compiler	
+Set compiler to use. If not set, pymake will look at your files and guess.  
+
+
--- pymake.py
+++ pymake.py
@@ -6,19 +6,22 @@ from optparse import OptionParser
 flags = ""
 outputFile = ""
 directory = ""
+compiler = ""
 
 def parseCommandline():
-    global flags, outputFile, directory 
+    global flags, outputFile, directory, compiler
     # Get commandline arguments
-    parser = OptionParser(usage="Usage: pymake.py [-f \"Flags\" -o Output File -d Directory]", version="PyMake Version 0.1.2")
+    parser = OptionParser(usage="Usage: pymake.py [-f \"Flags\" -o Output File -d Directory -c Compiler]", version="PyMake Version 0.1.3")
     parser.add_option("-f", "--flags", dest="flags", help="Extra flags for the compiler. Typed within quotes. For example, \"-Wall\". Default is \"-o [fileName]\"", default="", metavar="Flags")
     
     parser.add_option("-o", "--output", dest="outputFile", help="Output file name from compiler. Default is a.out", default="a.out", metavar="outputFile") 
     parser.add_option("-d", "--directory", dest="directory", help="Directory for pymake to create Makefile for. Default is ./", default="./", metavar="directory") 
+    parser.add_option("-c", "--compiler", dest="compiler", help="Set compiler to use. If not set, pymake will look at your files and guess.", default="", metavar="compiler") 
     (options, args) = parser.parse_args()
     outputFile = options.outputFile
     flags = options.flags
     directory = options.directory
+    compiler = options.compiler
 
 def files():
     allFiles = [f for f in os.listdir('.') if os.path.isfile(f)]
@@ -34,7 +37,7 @@ def typeOfFile(fileName):
 def baseFileName(fileName):
     return fileName[:(fileName.index('.'))]
 
-def compiler(fileType):
+def compilerName(fileType):
     if fileType == "c":
         return "gcc"
     elif fileType == "cpp":
@@ -57,6 +60,8 @@ def generateFileContents(fileNames, compilerName):
     fileContents += "\n\n"
     fileContents += "clean:" + "\n"
     fileContents += "\t" + "$(RM) " + outputFile
+
+    fileContents += "\n"
     return fileContents 
 
 def writeToMakefile(fileContents):
@@ -66,13 +71,18 @@ def writeToMakefile(fileContents):
 
 
 def start():
+    global compiler
     parseCommandline()
     os.chdir(directory)
     fileList = files()
+    fileContents = ""
     fileNames = ""
     for f in fileList:
         fileNames += f + " "
-    fileContents = generateFileContents(fileNames, compiler(typeOfFile(fileList[0]))) # Take the file type of the first file
+    if compiler is "":
+        fileContents = generateFileContents(fileNames, compilerName(typeOfFile(fileList[0]))) # Take the file type of the first file
+    else:
+        fileContents = generateFileContents(fileNames, compiler) # Use the user specified compiler
     writeToMakefile(fileContents)
     print("Makefile created.")
     exit(0)
